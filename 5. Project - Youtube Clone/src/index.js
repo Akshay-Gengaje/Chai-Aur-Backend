3e657cec7962769619e321584e0a6ee859b80a04
Akshay-Gengaje <Akshaysgengaje@gmail.com>
2024-06-05 08:10:11 +0530

    Initial setup done

--- 5. Project - Youtube Clone/src/index.js
+++ 5. Project - Youtube Clone/src/index.js
@@ -3,11 +3,17 @@ import dotenv from "dotenv";
 import connectDB from "./db/index.js";
 
 dotenv.config({
-    path : "./env"
+  path: "./env",
 });
 const app = express();
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT, () => {
+      console.log(`Server is running on port ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => console.log("Mongodb connection failed !!!" + err));
 // (aync () =>{
 //     try {
 //         mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
