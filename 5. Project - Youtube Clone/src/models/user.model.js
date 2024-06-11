7315bbc601fad9b0305551d687149fa628b5abf6
Akshay-Gengaje <Akshaysgengaje@gmail.com>
2024-06-11 09:21:29 +0530

    JWT Access token and refresh token methods created

--- 5. Project - Youtube Clone/src/models/user.model.js
+++ 5. Project - Youtube Clone/src/models/user.model.js
@@ -1,5 +1,6 @@
 import { Schema, model } from "mongoose";
-
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
 const userSchema = new Schema(
   {
     userName: {
@@ -51,4 +52,40 @@ const userSchema = new Schema(
   }
 );
 
+userSchema.pre("save", async function (next) {
+  // check the passowrd field and if it is present or modified then skip the line below
+  if (!this.isModified("password")) return next();
+  this.password = bcrypt.hash(this.password, 10);
+  next();
+});
+
+userSchema.methods.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+userSchema.methods.generateAccessToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+      email: this.email,
+      userName: this.userName,
+      fullName: this.fullName,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+    }
+  );
+};
+userSchema.methods.refreshAccessToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+      expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
+    }
+  );
+};
 export const User = model("User", userSchema);
